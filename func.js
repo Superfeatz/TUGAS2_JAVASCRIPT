66851d48fc6cc29511a24dd4faa765dd8063913f
Superfeatz <144724510+Superfeatz@users.noreply.github.com>
2023-09-26 11:04:39 +0700

    Add files via upload

---
+++ func.js
@@ -0,0 +1,31 @@
+// fungsi operasi penambahan
+function tambah() {
+    let angka1 = parseFloat(document.getElementById("angka1").value);
+    let angka2 = parseFloat(document.getElementById("angka2").value);
+    let hasil = angka1 + angka2;
+    document.getElementById("hasil").value = hasil;
+  }
+  
+  // fungsi operasi pengurangan
+  function kurang() {
+    let angka1 = parseFloat(document.getElementById("angka1").value);
+    let angka2 = parseFloat(document.getElementById("angka2").value);
+    let hasil = angka1 - angka2;
+    document.getElementById("hasil").value = hasil;
+  }
+  
+  // fungsi operasi perkalian
+  function kali() {
+    let angka1 = parseFloat(document.getElementById("angka1").value);
+    let angka2 = parseFloat(document.getElementById("angka2").value);
+    let hasil = angka1 * angka2;
+    document.getElementById("hasil").value = hasil;
+  }
+  
+  // fungsi operasi pembagian
+  function bagi() {
+    let angka1 = parseFloat(document.getElementById("angka1").value);
+    let angka2 = parseFloat(document.getElementById("angka2").value);
+    let hasil = angka1 / angka2;
+    document.getElementById("hasil").value = hasil;
+  }
